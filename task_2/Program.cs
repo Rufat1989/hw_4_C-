f6ba3b1f873143a11c2eda4368a1aceb86bd681c
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-27 16:17:27 +0600

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+int Factorial(int n)
+{
+    int sum = 0;
+       while(n > 0)
+    {
+        sum = sum + n % 10;
+        n = n / 10;
+        
+    }
+    return sum; 
+    
+}
+
+Console.WriteLine ("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"{n} --> {Factorial(n)}");
+
