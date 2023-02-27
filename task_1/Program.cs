0adf8f91d60dabd31eb19350831862d59f210b3a
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-27 15:15:18 +0600

    1_task

---
+++ task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int y = int.Parse(Console.ReadLine());
+
+int stepen(int x)
+{
+    int count = 1;
+    for(int i =0; i < y; i++)
+    count *= x;
+    return count;
+}
+Console.WriteLine(stepen(x));
