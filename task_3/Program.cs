d01085026bd0da016f55f2a081c20674c0fa3061
Rufat Karimov <ruffat_karimov@mail.ru>
2023-02-27 16:51:09 +0600

    task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = result(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]}, ");
+}
+
+
+int[] result(int n)
+{
+    int[] res = new int [n];
+    for(int i = 0; i < n; i++)
+    {
+        res[i] = new Random(). Next(0, 100);
+    }
+    return res;
+}
